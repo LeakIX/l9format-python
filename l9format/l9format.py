dad2666beb5dd92acaa9fc8bc76900d7bd05d8f1
Danny Willems <be.danny.willems@gmail.com>
2021-12-19 15:56:49 +0100

    Include Decimal fields for lat and lon
    It can be 0 which is not a valid float for JSON

--- l9format/l9format.py
+++ l9format/l9format.py
@@ -1,4 +1,54 @@
+import decimal
 from serde import Model, fields
+
+# Import from https://github.com/rossmacarthur/serde/commit/304884bca3c80e8b9a22a054b64dd94e3324b593#diff-dd2f65f16516311f0183377201a3d0b7894438787da732fc2bbc9c28cbde9fb8
+# A helper function...
+def round_decimal(
+    decimal_obj: decimal.Decimal, num_of_places: int = 6
+) -> decimal.Decimal:
+    return decimal_obj.quantize(decimal.Decimal(10) ** -num_of_places).normalize()
+
+
+class Decimal(fields.Instance):
+    """
+    A `~decimal.Decimal` field.
+    This field serializes `~decimal.Decimal` objects as strings and
+    deserializes string representations of Decimals as `~decimal.Decimal`
+    objects.
+    The resolution of the decimal can be specified. When not specified, the number
+    is not rounded. When it is specified, the decimal is rounded to this number of
+    decimal places upon serialization and deserialization.
+    Note: When float type numbers are not rounded before serialization,
+    they will be serialized in exact form, which as they are floats,
+    is almost never the exact intended value,
+    e.g. 0.2 = 0.20000000000000000000023
+    Args:
+        resolution (Union[int, bool]): The number of decimal places to round to.
+        When None, rounding is disabled.
+        **kwargs: keyword arguments for the `Field` constructor.
+    """
+
+    ty = decimal.Decimal
+
+    def __init__(self, resolution=None, **kwargs):
+        super(Decimal, self).__init__(self.__class__.ty, **kwargs)
+        self.resolution = resolution
+
+    def serialize(self, value: decimal.Decimal) -> str:
+        if self.resolution is not None:
+            value = round_decimal(value, num_of_places=self.resolution)
+        return "{0:f}".format(value)
+
+    def deserialize(self, value) -> decimal.Decimal:
+        try:
+            if self.resolution is not None:
+                return round_decimal(
+                    decimal.Decimal(value), num_of_places=self.resolution
+                )
+
+            return decimal.Decimal(value)
+        except decimal.DecimalException:
+            raise ValidationError("invalid decimal", value=value)
 
 
 class L9HttpEvent(Model):
@@ -7,7 +57,7 @@ class L9HttpEvent(Model):
     status: fields.Int()
     # FIXME: must be int64
     length: fields.Int()
-    header: fields.Dict(key=fields.Str(), value=fields.Str())
+    header: fields.Optional(fields.Dict(key=fields.Str(), value=fields.Str()))
     title: fields.Str()
     favicon_hash: fields.Str()
 
@@ -18,7 +68,7 @@ class ServiceCredentials(Model):
     password: fields.Str()
     key: fields.Str()
     # Bytes
-    raw: fields.Str()
+    raw: fields.Optional(fields.Str())
 
 
 class SoftwareModule(Model):
@@ -31,13 +81,13 @@ class Software(Model):
     name: fields.Str()
     version: fields.Str()
     os: fields.Str()
-    modules: fields.List(fields.Nested(SoftwareModule))
+    modules: fields.Optional(fields.List(fields.Nested(SoftwareModule)))
     fingerprint: fields.Str()
 
 
 class Certificate(Model):
     cn: fields.Str()
-    domain: fields.List(fields.Str())
+    domain: fields.Optional(fields.List(fields.Str()))
     fingerprint: fields.Str()
     key_algo: fields.Str()
     key_size: fields.Int()
@@ -48,8 +98,8 @@ class Certificate(Model):
 
 
 class GeoPoint(Model):
-    lat: fields.Float()
-    lon: fields.Float()
+    lat: Decimal()
+    lon: Decimal()
 
 
 class GeoLocation(Model):
@@ -75,7 +125,7 @@ class DatasetSummary(Model):
     size: fields.Int()
     collections: fields.Int()
     infected: fields.Bool()
-    ransom_notes: fields.List(fields.Str())
+    ransom_notes: fields.Optional(fields.List(fields.Str()))
 
 
 class L9LeakEvent(Model):
@@ -124,6 +174,6 @@ class L9Event(Model):
     ssh: fields.Nested(L9SSHEvent)
     service: fields.Nested(L9ServiceEvent)
     leak: fields.Nested(L9LeakEvent)
-    tags: fields.List(fields.Str())
+    tags: fields.Optional(fields.List(fields.Str()))
     geoip: fields.Nested(GeoLocation)
     network: fields.Nested(Network)
